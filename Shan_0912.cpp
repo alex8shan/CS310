1ee1168299e60a8610c0f504fb02c495fe734c40
Minghao Shan <minghao8shan@gmail.com>
2017-09-11 20:48:27 -0500

    Half finished; the result is always true

--- Shan_0912.cpp
+++ Shan_0912.cpp
@@ -1,38 +1,65 @@
-
-
 #include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <list>
+
+using namespace std;
+
 #include "Matrix.h"
 using namespace std;
 
-bool is_connected( const Matrix<uint8_t> &graph )
+bool is_connected( const Matrix<bool> &graph )
 {
-    vector< bool > reached( graph.numcols, false );
+    vector< bool > reached( graph.numcols(), false );
 
-    queue< uint8_t > to_be_explored;
+    queue< uint16_t > to_be_explored;
     to_be_explored.push( 0 );
     while ( !to_be_explored.empty() )
     {
-        uint8_t current_node = to_be_explored.front();
+        uint16_t current_node = to_be_explored.front();
         to_be_explored.pop();
+        uint16_t row_num = 0;
 
         reached.at( current_node ) = true;
-        for ( uint16_t col_num = 0; col_num <= graph.numcols(); col_num++ )
+        for ( uint16_t col_num = 0; col_num < graph.numcols(); col_num++ )
         {
-            for ( uint8_t adjacent : graph.at(row_num, col_num) )////What the fuck???
+            if(graph.at(row_num, col_num))
             {
-                if ( !reached.at(adjacent) )
+                uint16_t adjacent = col_num;
+                if ( !reached.at( adjacent) )
                 {
-                    to_be_explored.push(adjacent);
+                    to_be_explored.push( adjacent );
                 }
             }
         }
+        row_num++;
     }
     return find( reached.begin(), reached.end(), false ) == reached.end();
 }
 
 int main()
 {
-    // create matrix
-    Matrix(5, 5) graph;
+
+    Matrix <bool> graph (4,4);
     graph.at(0, 1) = true;
+    graph.at(0, 2) = true;
+    graph.at(0, 3) = true;
+    graph.at(1, 0) = true;
+    graph.at(1, 3) = true;
+    graph.at(2, 0) = true;
+    graph.at(3, 0) = true;
+    graph.at(3, 1) = true;
+
+    cout<<"first test " <<boolalpha << is_connected( graph ) << endl;
+
+    
+    Matrix <bool> graph1(4,4);
+    graph1.at(0, 1) = true;
+    graph1.at(1, 0) = true;
+    graph1.at(2, 3) = true;
+    graph1.at(3, 2) = true;
+
+    cout<<"second test " <<boolalpha << is_connected( graph ) << endl;
 }
